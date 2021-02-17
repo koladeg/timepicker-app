8749b89cdcf3a9b4a808ba556a5ad23c03cad610
koladeg <tayokay@yahoo.com>
2021-02-17 11:59:47 +0100

    initial commit

--- App.js
+++ App.js
@@ -1,21 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { Button, View, StyleSheet, Text, TouchableOpacity } from "react-native";
+import DateTimePickerModal from "react-native-modal-datetime-picker";
+import moment from 'moment';
 
-export default function App() {
+const App = () => {
+  const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
+  const [date, setDate] = useState('');
+
+  const showDatePicker = () => {
+    setDatePickerVisibility(true);
+  };
+
+  const hideDatePicker = () => {
+    setDatePickerVisibility(false);
+  };
+
+  const handleConfirm = (date) => {
+    console.warn("A date has been picked: ", date);
+    setDate(moment(date).format('MMMM Do YYYY, h:mm a'))
+    hideDatePicker();
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={styles.container}>
+        <Button style={styles.button} title="Show Date Picker" onPress={showDatePicker} />
+        <TouchableOpacity onPress={showDatePicker}>
+          <Text style={{fontSize: 30, color: 'blue',}}>{date}</Text>
+        </TouchableOpacity>
+        <DateTimePickerModal
+            isVisible={isDatePickerVisible}
+            mode="datetime"
+            onConfirm={handleConfirm}
+            onCancel={hideDatePicker}
+            is24Hour={false}
+        />
+      </View>
   );
-}
+};
+
+export default App;
 
 const styles = StyleSheet.create({
-  container: {
+  container:{
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems:'center',
   },
-});
+  button: {
+    width: 250,
+    height: 50,
+    backgroundColor: '#330066',
+    borderRadius: 30,
+    justifyContent: 'center',
+    marginTop: 15
+  }
+})
